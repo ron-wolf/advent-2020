5570977e447d080cd0ab54536d909b06f2c2b075
Ron Wolf <ron-wolf@users.noreply.github.com>
2020-12-01 18:22:32 -0500

    Revise 1.2: include third complement

--- 2020-12-01/puzzle-2-solution.jl
+++ 2020-12-01/puzzle-2-solution.jl
@@ -1,13 +1,15 @@
 using DelimitedFiles: readdlm
 
 expenses = readdlm("puzzle-1-input.txt", Int)
+matches = zeros(Int, 3)
 complements1 = 2020 .- expenses
-for complement in complements1
-	complements2 = complement .- complements1
+for matches[1] in (complements1)
+	complements2 = matches[1] .- complements1
 	common = intersect(complements2, expenses)
 	if ! isempty(common)
-		prod_of_matches = complement * prod(common)
-		println(prod_of_matches)
+		matches[2] = first(common)
+		matches[3] = 2020 - sum(matches)
+		println(prod(matches))
 		break
 	end
 end
