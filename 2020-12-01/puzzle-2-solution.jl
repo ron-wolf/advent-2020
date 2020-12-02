52731b45e11cbb5addd3fe9cbfa978b1756ec38a
Ron Wolf <ron-wolf@users.noreply.github.com>
2020-12-01 20:30:20 -0500

    Minor 1.2: remove unused vars

--- 2020-12-01/puzzle-2-solution.jl
+++ 2020-12-01/puzzle-2-solution.jl
@@ -1,11 +1,10 @@
 using DelimitedFiles: readdlm
 
 expenses = readdlm("puzzle-1-input.txt", Int)
-matches = zeros(Int, 3)
 remainders = 2020 .- expenses # numbers that could be added to an expense to get 2020
 for rem in remainders # one such number
 	complements = rem .- expenses # numbers that could be added to an expense to get rem
-	matches = intersect(complements, expenses) # only such numbers that are also in rem
+	matches = intersect(complements, expenses) # only such numbers that are also in expenses
 	if ! isempty(matches)
 		orig = 2020 - rem
 		push!(matches, orig)
