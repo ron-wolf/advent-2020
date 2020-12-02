da1556be070ea2e467afae2df5e216b56b208f61
Ron Wolf <ron-wolf@users.noreply.github.com>
2020-12-01 20:27:40 -0500

    Refactor 1.2: no more brute force

--- 2020-12-01/puzzle-2-solution.jl
+++ 2020-12-01/puzzle-2-solution.jl
@@ -1,5 +1,15 @@
 using DelimitedFiles: readdlm
 
 expenses = readdlm("puzzle-1-input.txt", Int)
-matches = first( (x, y, z) for x in expenses, y in expenses, z in expenses if x+y+z==2020 )
-println(prod(triplet))
+matches = zeros(Int, 3)
+remainders = 2020 .- expenses # numbers that could be added to an expense to get 2020
+for rem in remainders # one such number
+	complements = rem .- expenses # numbers that could be added to an expense to get rem
+	matches = intersect(complements, expenses) # only such numbers that are also in rem
+	if ! isempty(matches)
+		orig = 2020 - rem
+		push!(matches, orig)
+		println(prod(matches))
+		break
+	end
+end
