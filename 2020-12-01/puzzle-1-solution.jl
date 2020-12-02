47dcef071c5cb8c3889e1d45335fdbeae8f453d9
Ron Wolf <ron-wolf@users.noreply.github.com>
2020-12-01 20:28:16 -0500

    Annotate 1.1

--- 2020-12-01/puzzle-1-solution.jl
+++ 2020-12-01/puzzle-1-solution.jl
@@ -1,6 +1,7 @@
 using DelimitedFiles: readdlm
 
 expenses = readdlm("puzzle-1-input.txt", Int)
-complements = 2020 .- expenses
-matches = intersect(expenses, complements)
+complements = 2020 .- expenses # numbers that could be added to an expense to get 2020
+matches = intersect(expenses, complements) # only such numbers that are also in expenses
+# there are exactly two such numbers: the unique pair that adds to 2020
 println(prod(matches))
