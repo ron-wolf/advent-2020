7af8c3bd1a0bd3655f98789bb1ec6f35c6bf9d41
Ron Wolf <ron-wolf@users.noreply.github.com>
2020-12-01 19:28:25 -0500

    Revise 1.2: K.I.S.S.

--- 2020-12-01/puzzle-2-solution.jl
+++ 2020-12-01/puzzle-2-solution.jl
@@ -1,15 +1,5 @@
 using DelimitedFiles: readdlm
 
 expenses = readdlm("puzzle-1-input.txt", Int)
-matches = zeros(Int, 3)
-complements1 = 2020 .- expenses
-for matches[1] in (complements1)
-	complements2 = matches[1] .- complements1
-	common = intersect(complements2, expenses)
-	if ! isempty(common)
-		matches[2] = first(common)
-		matches[3] = 2020 - sum(matches)
-		println(prod(matches))
-		break
-	end
-end
+triplet = first( (x, y, z) for x in expenses, y in expenses, z in expenses if x+y+z==2020 )
+println(prod(triplet))
