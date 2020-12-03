d3581cc00634648044ad8f50e530a8b7fc93b8fb
Ron Wolf <ron-wolf@users.noreply.github.com>
2020-12-03 01:57:43 -0500

    Init 3.2

---
+++ 2020-12-03/puzzle-2-solution.jl
@@ -0,0 +1,18 @@
+rows = (collect(line) .== '#' for line in eachline("puzzle-1-input.txt"))
+matrix = transpose(hcat(rows...))
+
+slopes = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]]
+const (len, wid) = size(matrix)
+
+trees = map(slopes) do slope
+	pos = [1, 1]
+	total = 0
+	while pos[1] <= len
+		total += matrix[CartesianIndex(pos...)]
+		pos .+= slope
+		pos[2] = (pos[2]-1)%wid + 1
+	end
+	total
+end
+
+println(prod(trees))
