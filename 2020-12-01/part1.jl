b764afc289d3db6f1e27c97934372330c32377ee
removedporn <86824510+removedporn@users.noreply.github.com>
2023-06-11 18:33:33 +0800

    Delete 2020-12-01 directory

--- 2020-12-01/part1.jl
+++
@@ -1,8 +0,0 @@
-using DelimitedFiles: readdlm
-
-inputfile = joinpath(@__DIR__, "input.txt")
-expenses = vec(readdlm(inputfile, Int))
-complements = 2020 .- expenses # numbers that could be added to an expense to get 2020
-matches = intersect(expenses, complements) # only such numbers that are also in expenses
-# there are exactly two such numbers: the unique pair that adds to 2020
-println(prod(matches))
